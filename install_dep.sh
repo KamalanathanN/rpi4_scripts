ab31347d40a1eabae439a558a909bfe7c89cd6df
KamalanathanN <31615486+KamalanathanN@users.noreply.github.com>
2021-03-29 20:49:27 +0530

    Initial commit

---
+++ install_dep.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [[ $(grep 20.04 /etc/issue) ]]; then
+    # Install ROS dependencies (for Foxy on Ubuntu 20.04)
+    sudo apt install -y python3-rosdep
+else
+    # Install ROS dependencies (for Dashing and Eloquent on Ubuntu 18.04)
+    sudo apt install -y python-rosdep
+fi
+
+    # Install Common Linux Tools
+    sudo apt install -y \
+	    build-essential \
+	    cmake \
+	    git \
+	    libbullet-dev \
+	    python3-argcomplete \
+	    python3-colcon-common-extensions \
+	    python3-flake8 \
+	    python3-pip \
+	    python3-pytest-cov \
+	    python3-setuptools \
+	    python3-vcstool \
+	    openssh-server \
+	    byobu \
+	    wget
+
+    # Initialize and update rosdep
+    sudo rosdep init
+    rosdep update
+
+echo "ROS-related packages installed successfully"
